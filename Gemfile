6daa53c5264003b381ae79ab643ca829e59b80e7
Michael Camilleri <mike@inqk.net>
2020-01-18 09:07:03 +0900

    Add asset pipeline

--- Gemfile
+++ Gemfile
@@ -1,4 +1,8 @@
 source 'https://rubygems.org'
 
-gem 'sinatra',  '~>2.0'
 gem 'rouge'
+
+gem 'sinatra',  '~>2.0'
+gem 'sprockets', '~>4.0'
+gem 'uglifier', '~>4.0'
+gem 'sassc', '~>2.0'
--- app.rb
+++ app.rb
@@ -1,7 +1,27 @@
 require 'rouge'
 require 'sinatra/base'
+require 'sprockets'
+require 'uglifier'
+require 'sassc'
 
 class Dingus < Sinatra::Base
+  # initialize new sprockets environment
+  set :environment, Sprockets::Environment.new
+
+  # append assets paths
+  environment.append_path "assets/stylesheets"
+  environment.append_path "assets/javascripts"
+
+  # compress assets
+  environment.js_compressor  = :uglify
+  environment.css_compressor = :scssc
+
+  # get assets
+  get "/assets/*" do
+    env["PATH_INFO"].sub!("/assets", "")
+    settings.environment.call(env)
+  end
+
   get '/' do
     erb :index
   end
