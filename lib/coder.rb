833f7e7173713f87e956f05647c469a60a354e1d
Michael Camilleri <mike@inqk.net>
2020-01-23 23:38:07 +0900

    Implement Coder.decode method

--- lib/coder.rb
+++ lib/coder.rb
@@ -1,9 +1,18 @@
 class Coder
-  def self.encode(text, rotation = 6)
+  DEFAULT_ROTATION = 6
+  BOUND = 0x10000
+
+  def self.decode(text, rotation = DEFAULT_ROTATION)
     return text if text.empty? || rotation == 0
-    bound = 0x10000
     text.codepoints.map do |n|
-      ((n + rotation + bound) % bound).chr(Encoding::UTF_8)
+      ((n - rotation) % BOUND).chr(Encoding::UTF_8)
+    end.join
+  end
+
+  def self.encode(text, rotation = DEFAULT_ROTATION)
+    return text if text.empty? || rotation == 0
+    text.codepoints.map do |n|
+      ((n + rotation) % BOUND).chr(Encoding::UTF_8)
     end.join
   end
 end
--- test/coder_test.rb
+++ test/coder_test.rb
@@ -8,6 +8,30 @@ class CoderTest < Minitest::Test
   def setup
   end
 
+  def test_decode_with_no_rotation
+    assert_equal "abc", Coder.decode("abc", 0)
+  end
+
+  def test_decode_with_custom_rotation
+    assert_equal "abc", Coder.decode("def", 3)
+  end
+
+  def test_decode_with_empty_string
+    assert_equal "", Coder.decode("")
+  end
+
+  def test_decode_with_latin
+    assert_equal "abc", Coder.decode("ghi")
+  end
+
+  def test_decode_with_symbols
+    assert_equal "?!'", Coder.decode("E'-")
+  end
+
+  def test_decode_with_japanese
+    assert_equal "テスト", Coder.decode("ヌタノ")
+  end
+
   def test_encode_with_no_rotation
     assert_equal "abc", Coder.encode("abc", 0)
   end
