ee8a3098c2cbafb802b4cbfca64de75813eacfa1
Michael Camilleri <mike@inqk.net>
2020-01-23 17:58:23 +0900

    Implement Coder.encode method

--- lib/coder.rb
+++ lib/coder.rb
@@ -1,2 +1,9 @@
 class Coder
+  def self.encode(text, rotation = 6)
+    return text if text.empty? || rotation == 0
+    bound = 0x10000
+    text.codepoints.map do |n|
+      ((n + rotation + bound) % bound).chr(Encoding::UTF_8)
+    end.join
+  end
 end
--- test/coder_test.rb
+++ test/coder_test.rb
@@ -7,4 +7,28 @@ require_relative "../lib/coder"
 class CoderTest < Minitest::Test
   def setup
   end
+
+  def test_encode_with_no_rotation
+    assert_equal "abc", Coder.encode("abc", 0)
+  end
+
+  def test_encode_with_custom_rotation
+    assert_equal "def", Coder.encode("abc", 3)
+  end
+
+  def test_encode_with_empty_string
+    assert_equal "", Coder.encode("")
+  end
+
+  def test_encode_with_latin
+    assert_equal "ghi", Coder.encode("abc")
+  end
+
+  def test_encode_with_symbols
+    assert_equal "E'-", Coder.encode("?!'")
+  end
+
+  def test_encode_with_japanese
+    assert_equal "ヌタノ", Coder.encode("テスト")
+  end
 end
